a204c9ba5fb97041d5b1083b5fb9ea07ef372dbe
Kingrashy12 <www.rashyking05@gmail.com>
2025-02-08 00:50:11 +0100

    update calculator.sol

--- learning/calculator.sol
+++ learning/calculator.sol
@@ -1,23 +1,22 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 contract Calculator {
-   uint256 private result = 0;
+  uint256 private result = 0;
 
-   function add(uint256 num) public {
+  function add(uint256 num) public {
     result += num;
-   }
+  }
 
-   function subtract(uint256 num) public {
+  function subtract(uint256 num) public  {
     result -= num;
-   }
+  }
 
-   function mutiplay(uint256 num) public {
+  function multiply(uint256 num) public {
     result *= num;
-   }
+  }
 
-   function get() public view returns (uint256) {
+  function get() public view returns (uint256) {
     return result;
-   }
+  }
 }
