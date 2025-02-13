4265af423057a5ca7a8cd4ebc593f21f5aa22a32
Kingrashy12 <www.rashyking05@gmail.com>
2025-02-13 14:11:20 +0100

    Update fundme.sol

--- learning/contracts/FundMe.sol
+++ learning/contracts/FundMe.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import { PriceConverter } from "../lib/PriceConverter.sol";
 
 contract FundMe {
-    
     using PriceConverter for uint256;
-
-    uint public minimumUSD = 5e18;
-    address[] public founders;
+    address ETHUSD = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    uint256 minimumUsd = 5e18;
+    address[] funders;
     mapping (address => uint256) public addressToAmountFounded;
 
-    address ETHUSD = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-
-    function fund() public payable {
-     require(msg.value.getConversationRate(ETHUSD) >= minimumUSD,"Please send mimimun ETH");
-     founders.push(msg.sender);
-     addressToAmountFounded[msg.sender] = addressToAmountFounded[msg.sender] + msg.value;
+    function fund() public payable  {
+        require(msg.value.getConversationRate(ETHUSD) >= minimumUsd,"You can't send below $5");
+        funders.push(msg.sender);
+        addressToAmountFounded[msg.sender] += msg.value;
     }
 
-   
-
+    function withdraw() public {
+        for(uint256 funderIndex; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFounded[funder] = 0;
+        }
+    }
 }
